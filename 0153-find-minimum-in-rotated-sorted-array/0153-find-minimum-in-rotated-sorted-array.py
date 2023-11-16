da3b391b63902d51e9c625276c906d8271e5a596
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-11-16 22:56:04 +0530

    Time: 41 ms (89.55%), Space: 16.7 MB (15.63%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,7 +4,10 @@ class Solution:
         Time: O(logn), where n is len(nums)
         Space: O(1)
         
-        We are basically doing binary search where we are eliminating one half at a time. Instead of the standard binary search of finding some target, we are basically eliminating a half when we know that the half is sorted (is in increasing order). We then continue till we reach a single element and exit the loop.
+        We are basically doing binary search where we are eliminating one half at a time. 
+        Instead of the standard binary search of finding some target, we are basically eliminating a half 
+        when we know that the half is sorted (is in increasing order). We then continue till we 
+        reach a single element and exit the loop.
         """
         l, r = 0, len(nums)-1
         output = math.inf
