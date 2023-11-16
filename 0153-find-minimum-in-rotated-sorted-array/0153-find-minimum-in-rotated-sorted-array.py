fee8460a4d5534ee40dd2306879b20e551a4090f
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-11-16 23:10:36 +0530

    Time: 51 ms (26.01%), Space: 16.6 MB (15.63%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -7,7 +7,8 @@ class Solution:
         We are basically doing binary search where we are eliminating one half at a time. 
         Instead of the standard binary search of finding some target, we are basically eliminating a half 
         when we know that the half is sorted (is in increasing order). We then continue till we 
-        reach a single element and exit the loop.
+        reach a single element and exit the loop. We are eliminating one sorted half at a time since we already
+        know that the first element of the sorted half will be the smallest.
         """
         l, r = 0, len(nums)-1
         output = math.inf
@@ -18,6 +19,7 @@ class Solution:
             if nums[l] < nums[m]:
                 output = min(output, nums[l])
                 l = m + 1
+            # If left is greater than or equal to mid, then the left half is not sorted and the right half is sorted
             else:
                 output = min(output, nums[m])
                 r = m - 1
