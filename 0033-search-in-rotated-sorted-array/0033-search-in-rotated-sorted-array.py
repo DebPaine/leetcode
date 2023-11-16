1468e8c50e3f0c811d37ced87dc7a80362eded2e
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-11-17 00:49:49 +0530

    Time: 58 ms (6.48%), Space: 16.7 MB (39.21%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        Time: O(n), where n is len(nums)
+        Space: O(1)
+
+        Very similar to problem 153. We are first identifying which half of the array is sorted
+        and then going through it and trying to find the target.
+        """
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            m = r - (r-l)//2
+
+            if target == nums[m]:
+                return m
+            if nums[l] < nums[m]:
+                if nums[l] <= target <= nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] <= target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return -1
+                    
