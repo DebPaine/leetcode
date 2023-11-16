f74a8174ee95fd822d3aea4d5d162ad2d44a8837
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-11-16 22:55:31 +0530

    Time: 37 ms (97.16%), Space: 16.6 MB (15.63%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,10 +1,17 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        """
+        Time: O(logn), where n is len(nums)
+        Space: O(1)
+        
+        We are basically doing binary search where we are eliminating one half at a time. Instead of the standard binary search of finding some target, we are basically eliminating a half when we know that the half is sorted (is in increasing order). We then continue till we reach a single element and exit the loop.
+        """
         l, r = 0, len(nums)-1
         output = math.inf
 
         while l <= r:
             m = r - (r-l)//2
+            # If left is less than mid, then the left half is sorted and we can eliminate that
             if nums[l] < nums[m]:
                 output = min(output, nums[l])
                 l = m + 1
