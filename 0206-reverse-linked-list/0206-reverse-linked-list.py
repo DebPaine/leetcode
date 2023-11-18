3422edbb611696e9b9038cd1cb7511cf80e688f8
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-11-18 20:26:34 +0530

    Time: 47 ms (23.80%), Space: 23 MB (6.16%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -36,6 +36,7 @@ class Solution:
         head.next.next = head   # if head is 4, head.next = 5, 5.next should point back to 4 so that it's reversed
         head.next = None   # now head is 4, so it's pointing to 5 and 5 is pointing back to 4, and 3 is pointing to 4 too, so we get rid of 4.next
         """we return the new head which is 5 (the last node) in every recursive call since we want to pass the new head 
-        from the top of the call stack (when we reach the last node) till the bottom of the stack (when we reach the first node) so that we can get the starting node of this reversed linked list
+        from the top of the call stack (when we reach the last node) till the bottom of the 
+        stack (when we reach the first node) so that we can get the starting node of this reversed linked list
         """ 
         return new_head   
