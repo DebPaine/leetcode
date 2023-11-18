ab2b23d66dca4e898531a32bcbeda0e418da67c5
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-11-18 20:23:37 +0530

    Time: 46 ms (30.28%), Space: 23 MB (6.16%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Iterative solution
+        # curr = head
+        # prev = None
+
+        # while curr:
+        #     next_node = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = next_node
+
+        # return prev
+
+
+        # Recursive solution (try out visually, much easier to understand)
+        if not head:   # for scenario where user enters empty or null linked list
+            return None
+        if not head.next:   # for scenario where we reach the end of a linked list
+            return head
+
+        new_head = self.reverseList(head.next)   # reach the end of the linked list and get the new head
+        head.next.next = head   # if head is 4, head.next = 5, 5.next should point back to 4 so that it's reversed
+        head.next = None   # now head is 4, so it's pointing to 5 and 5 is pointing back to 4, and 3 is pointing to 4 too, so we get rid of 4.next
+        return new_head   # we return the new head which is 5 (the last node) in every recursive call since we want to pass the new head from the top of the call stack (when we reach the last node) till the bottom of the stack (when we reach the first node) so that we can get the starting node of this reversed linked list
