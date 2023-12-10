80ab14517a11cabd868880ceb605769a4bbf7bd0
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-12-10 18:07:37 +0530

    Time: 47 ms (27.27%), Space: 17.4 MB (24.57%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -15,19 +15,20 @@ class Solution:
 
         We are using a two-pass strategy. First pass is to store all the nodes as copy
         in our hashmap. Second pass is to link the next and random pointers to the nodes.
+        We basically store the curr and new node mapping in our hashmap so that in the second
+        pass we can rebuild all the next and random links.
         """
-        copy = { None: None }  # for cases where curr.next or curr.random is pointing to null
+        hashmap = {None: None}  # for cases where curr.next or curr.random is pointing to null
         curr = head
 
         while curr:
-            copy[curr] = Node(curr.val)
+            hashmap[curr] = Node(curr.val)
             curr = curr.next
 
         curr = head
         while curr:
-            current_node = copy[curr]
-            current_node.next = copy[curr.next]
-            current_node.random = copy[curr.random]
+            hashmap[curr].next = hashmap[curr.next]
+            hashmap[curr].random = hashmap[curr.random]
             curr = curr.next
-        
-        return copy[head]
+
+        return hashmap[head]
