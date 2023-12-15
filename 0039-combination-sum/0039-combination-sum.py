d97db256d4ae5b5cb0d3f23b21dc38a13e6add3e
DebPaine <49160622+DebPaine@users.noreply.github.com>
2023-12-15 14:04:22 +0530

    Time: 71 ms (25.22%), Space: 16.4 MB (41.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -11,16 +11,37 @@ class Solution:
         """
         output = []
 
-        def backtrack(i, subset):
+        # def backtrack(i, subset):
+        #     if sum(subset) == target:
+        #         output.append(subset[:])
+        #         return
+        #     if i >= len(candidates) or sum(subset) >= target:
+        #         return
+            
+        #     backtrack(i, subset + [candidates[i]])  # include the current num as we can have duplicates
+        #     backtrack(i+1, subset + [])  # exclude the current num and move to the next one
+        #     return
+        
+        # backtrack(0, [])
+        # return output
+
+        # Create a decision tree to understand this. 
+        # For eg:
+        # We will have 2, then 2,2, then 2,2,2 which will backtrack since it's greater than target
+        # Then we will have 2,2,3, etc
+        # Then we will go back to 2, then 2,3, 2,3,3, etc
+        def backtrack(start, subset):
             if sum(subset) == target:
                 output.append(subset[:])
                 return
-            if i >= len(candidates) or sum(subset) >= target:
+
+            if sum(subset) > target:
                 return
-            
-            backtrack(i, subset + [candidates[i]])  # include the current num as we can have duplicates
-            backtrack(i+1, subset + [])  # exclude the current num and move to the next one
-            return
+
+            for i in range(start, len(candidates)):
+                subset.append(candidates[i])
+                backtrack(i, subset)
+                subset.pop()
         
         backtrack(0, [])
         return output
