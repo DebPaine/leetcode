cab62e0ec5bb4f99ce4ebf3a2204f54b8c75ca30
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-02-20 20:14:40 +0530

    Time: 781 ms (40.27%), Space: 27.3 MB (95.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,10 @@ class Solution:
         Time: O(n), where n is the len(prices)
         Space: O(1)
         """
-        min_price = prices[0] 
+        min_price = math.inf
         max_profit = 0
 
-        for price in prices[1:]:
+        for price in prices:
             min_price = min(min_price, price)  # key here is to find the min price till now, essentially using a pointer to keep track of min_price
             profit = price - min_price  # calculate profit using current day price and min_price
             max_profit = max(max_profit, profit)  # see if this profit is max or not
