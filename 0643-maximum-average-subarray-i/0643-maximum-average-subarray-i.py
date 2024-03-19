9161ae66f866df5d258175c4e1d1d969e53fa142
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-03-20 00:27:27 +0530

    Time: 916 ms (27.46%), Space: 28.4 MB (42.67%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if k > len(nums):  # k, or sliding window length should not be larger than length of nums
+            return -1.0
+
+        curr_sum = max_sum = sum(nums[:k])  # calculate the current sum inside sliding window
+        for i in range(1, len(nums)-k+1):
+            curr_sum += nums[i+k-1] - nums[i-1]   # add the next element and subtract the previous to current sum
+            max_sum = max(max_sum, curr_sum)
+            
+        return max_sum/k
+
+        
