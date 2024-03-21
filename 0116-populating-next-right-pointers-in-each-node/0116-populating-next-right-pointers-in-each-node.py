b3437c73d56b5ab03d3841a6bb938d82425fdbaf
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-03-22 00:44:05 +0530

    Time: 43 ms (86.33%), Space: 18 MB (73.28%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -21,15 +21,44 @@ class Solution:
         if not root:
             return None
 
-        q = deque([root])
-        while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    node.left.next = node.right  # connect the left and right children of the current node
-                if node.right and node.next:  # we explicitly don't have to check for node.right since node.left exists
-                    node.right.next = node.next.left  # if next node exists for current node, then traverse it 
-                if node.left: q.append(node.left)
-                if node.right: q.append(node.right)
+        # q = deque([root])
+        # while q:
+        #     for _ in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             node.left.next = node.right  # connect the left and right children of the current node
+        #         if node.right and node.next:  # we explicitly don't have to check for node.right since node.left exists
+        #             node.right.next = node.next.left  # if next node exists for current node, then traverse it 
+        #         if node.left: q.append(node.left)
+        #         if node.right: q.append(node.right)
                 
+        # return root
+
+        # BFS without a queue
+        dummy = Node(999999)
+        head = root  
+        while head:
+            prev = dummy
+            curr = head
+            while curr:
+                if curr.left:
+                    prev.next = curr.left
+                    prev = prev.next
+                if curr.right:
+                    prev.next = curr.right
+                    prev = prev.next
+                curr = curr.next
+            head = dummy.next
+            dummy.next = None
         return root
+
+
+
+
+
+
+
+
+
+
+
