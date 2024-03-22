c698d2b65334f6bb7d572635060d25d86e8d71ef
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-03-22 12:31:25 +0530

    Time: 45 ms (6.07%), Space: 16.5 MB (91.89%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -8,25 +8,20 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         """
         Time: O(n), where n is the no. of nodes in the tree
-        Space: O(max(width)), as we need to store all the nodes in a level while doing BFS
-
-        Basically, it's just a BFS and we are always taking the rightmost value for every level and appending to output
+        Space: O(n), or O(level with max nodes) ~= O(n/2) ~= O(n)
+        Algorithm: Do BFS and take the last element of each level and append it to output
         """
         if root is None:
-            return root
-        
+            return None
+
         output = []
-        queue = deque([root])
-        
-        while queue:
-            right_side_val = None
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                right_side_val = node.val
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            output.append(right_side_val)
-        
+        q = deque([root])
+        while q:
+            last_element = 0
+            for _ in range(len(q)):
+                node = q.pop()
+                last_element = node.val
+                if node.left: q.appendleft(node.left)
+                if node.right: q.appendleft(node.right)
+            output.append(last_element)
         return output
