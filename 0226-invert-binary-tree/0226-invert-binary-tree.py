7f19161ae8c2825abfcf33ced5243b67b19f1196
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-03-26 22:25:30 +0530

    Time: 38 ms (38.56%), Space: 16.6 MB (34.82%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -7,41 +7,13 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         """
-        Time: O(n), where n is the number of nodes
-        Space: O(n) worst case if tree is linked list, else if it's balanced then it's O(logn) 
+        Time: O(n), where n is the no. of nodes in the tree
+        Space: O(n) if tree is LL, O(logn) if tree is balanced
         """
-        # Recursive DFS
-        # if not root:
-        #     return None
-        # root.left, root.right = root.right, root.left
-        # self.invertTree(root.left)
-        # self.invertTree(root.right)
-        # return root
-
-        
-        # Iterative DFS 
-        # if not root:
-        #     return None
-        # stack = [root]
-        # while stack:
-        #     node = stack.pop()
-        #     node.left, node.right = node.right, node.left
-        #     if node.right:
-        #         stack.append(node.right)
-        #     if node.left:
-        #         stack.append(node.left)
-        # return root
-
-
-        # Iterative BFS
         if not root:
             return None
-        queue = deque([root])
-        while queue:
-            node = queue.pop()
-            node.left, node.right = node.right, node.left
-            if node.left:
-                queue.appendleft(node.left)
-            if node.right:
-                queue.appendleft(node.right)
+        
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
