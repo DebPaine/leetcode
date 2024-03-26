29e323c268fd9da37b4928f4b49f05b588861284
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-03-26 22:16:09 +0530

    Time: 28 ms (98.60%), Space: 17.8 MB (9.53%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -7,58 +7,17 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         """
-        Time: O(n), where n is the number of nodes in the tree
-        Space: O(n) worst case if tree is linked list, else O(logn) for balanced binary tree
+        Time: O(n), where n is the no. of nodes in the tree
+        Space: O(n) is tree if LL, O(logn) if tree is balanced
+
+        Algorithm:
+        We just have to reach the leaf nodes and keep adding one every time we go 
+        up the tree. We then take the max(left, right) subtrees and add 1 to it to 
+        get the height of the current node.
         """
-        # Recursive DFS
-        # if not root:
-        #     return 0
-        # left = 1 + self.maxDepth(root.left)
-        # right = 1 + self.maxDepth(root.right)
-        # return max(left, right)
-
-
-        # Iterative DFS
-        # if not root:
-        #     return 0
-        # stack = [(root, 1)]
-        # max_depth = 0
-        # while stack:
-        #     node, depth = stack.pop()
-        #     max_depth = max(max_depth, depth)
-        #     if node.right:
-        #         stack.append((node.right, depth + 1))
-        #     if node.left:
-        #         stack.append((node.left, depth + 1))
-        # return max_depth
-
-
-        # Iterative BFS
-        # if not root:
-        #     return 0
-        # queue = deque([root])
-        # max_levels = 0
-        # while queue:
-        #     max_levels += 1
-        #     for _ in range(len(queue)):
-        #         node = queue.pop()
-        #         if node.left:
-        #             queue.appendleft(node.left)
-        #         if node.right:
-        #             queue.appendleft(node.right)
-        # return max_levels
-
-
-        # Alternative Iterative BFS
-        if not root:
+        if root is None:
             return 0
-        queue = deque([(root, 1)])
-        max_level = 0
-        while queue:
-            node, level = queue.pop()
-            max_level = max(max_level, level)
-            if node.left:
-                queue.appendleft((node.left, level + 1))
-            if node.right:
-                queue.appendleft((node.right, level + 1))
-        return max_level
+
+        left = 1 + self.maxDepth(root.left)
+        right = 1 + self.maxDepth(root.right)
+        return max(left, right)
