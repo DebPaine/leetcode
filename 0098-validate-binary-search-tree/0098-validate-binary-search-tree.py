7b6bf59768bd5ec0bf62299bb293f14fe0f41ad4
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-03-29 19:51:44 +0530

    Time: 47 ms (22.43%), Space: 18.4 MB (13.20%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,20 +7,23 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         """
-        Time: O(n), where n is the number of nodes in the tree
-        Space: O(n), if bst is like LL, else O(logn) if bst is balanced
-        
-        Instead of comparing current node's val with left and right children, we can just keep updating the 
-        lower and upper boundaries of current node's val.
+        Time: O(n), where n is the no. of nodes in the tree
+        Space: O(n), if tree is LL, else O(logn) if tree is balanced
+
+        Algorithm:
+        Our first thought will be to go down the tree using dfs and compare left and right
+        node values with parent node value. But we can't directly access the parent node. So,
+        we need to keep track of the lower and upper limit for each node as we go down the tree,
+        essentially we are just keep track of the boundary between which the node value is allowed.
         """
         def dfs(node, lower, upper):
-            if not node:  # a null node is a valid BST
+            if not node:
                 return True
-            if not (lower < node.val < upper):  # check the current node's val with lower and upper values
-                return False 
-
-            left = dfs(node.left, lower, node.val)   # left child has to be lesser than current node.val
-            right = dfs(node.right, node.val, upper) # right child has to be greater than current node.val
-            return left and right
+            if not (lower < node.val < upper):
+                return False
             
+            left = dfs(node.left, lower, node.val)
+            right = dfs(node.right, node.val, upper)
+            return left and right
+        
         return dfs(root, -math.inf, math.inf)
