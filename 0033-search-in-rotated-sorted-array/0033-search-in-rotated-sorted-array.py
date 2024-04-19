07e99205c53e0f2731525c2ab12d45e6e317d764
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-19 19:11:44 +0530

    Time: 46 ms (39.55%), Space: 17 MB (15.26%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,33 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         """
-        Time: O(logn), where n is len(nums)
+        Time: O(logn)
         Space: O(1)
 
+        Algorithm:
         Very similar to problem 153. We are first identifying which half of the array is sorted
         and then going through it and trying to find the target. Either the left or the right half 
         will always be sorted as the pivot will fall in one of the halves, so the other half will
         automatically be sorted.
+        Steps:
+        1. Check if left (or right) array half is sorted or not.
+        2. If it's not sorted, then the other half will definitely be sorted.
+        3. Search the other half using binary search for the target.
         """
-
         l, r = 0, len(nums)-1
 
         while l <= r:
-            m = r - (r-l)//2
-
-            if target == nums[m]:
-                return m
-            # If the left half is sorted, then we can find the element within it
-            if nums[l] <= nums[m]:   # can use < too since there are no duplicate elements in nums
-                if nums[l] <= target <= nums[m]:
-                    r = m - 1
+            mid = r - (r - l)//2
+            if target == nums[mid]:
+                return mid
+            # check if left half is sorted or not
+            if nums[l] < nums[mid]:
+                if nums[l] <= target <= nums[mid]:  # equal sign here since target can nums[l] or nums[mid]
+                    r = mid - 1
                 else:
-                    l = m + 1
-            # If the left half is not sorted, then the right half will be sorting and we can search there
+                    l = mid + 1
+            # if left half is not sorted, then the right half will automatically be sorted
             else:
-                if nums[m] <= target <= nums[r]:
-                    l = m + 1
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
                 else:
-                    r = m - 1
+                    r = mid - 1
         return -1
-                    
