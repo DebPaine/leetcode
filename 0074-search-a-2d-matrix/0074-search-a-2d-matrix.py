27d7d907065c6e69ce9147e31ac94cffa9d390c1
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-20 19:06:08 +0530

    Time: 39 ms (91.11%), Space: 17.1 MB (32.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,51 +1,56 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         """
-        Time: O(log(m)+log(n)) or O(log(m*n)), where m is no. of rows and n is no. of cols
+        Time: O(log(m)+log(n)) = O(log(m*n))
         Space: O(1)
+
+        Algorithms:
+        Both the approaches have the same time complexity. In the second approach, we just need
+        to use only one binary search instead of two in the first one. In first approach, we first
+        find the correct row, then we try to find the target in the row. In second approach, we treat
+        the entire matrix as a row of size m*n and apply binary search on it.
         """
-        # Instead of doing two binary searches, we can treat the entire matrix as a single sorted array
-        # rows, cols = len(matrix), len(matrix[0])
-        # l, r = 0, rows*cols-1
+        # # Approach 1: Using two binary searches, one to find the correct row, and the second one to find the element within the row
+        # top, bottom = 0, len(matrix)-1
+        # row = 0
+        # # Binary search to find the correct row
+        # while top <= bottom:
+        #     m = bottom - (bottom - top)//2
+        #     if matrix[m][0] <= target <= matrix[m][-1]:
+        #         row = m
+        #         break
+        #     if target > matrix[m][-1]:
+        #         top = m + 1
+        #     elif target < matrix[m][0]:
+        #         bottom = m - 1
 
+        # # Binary search to find the target in the above row that we found 
+        # l, r = 0, len(matrix[row])-1
         # while l <= r:
-        #     mid = r - (r  - l)//2
-        #     # Find which row and col the mid index belongs to
-        #     row, col = mid // cols, mid % cols
-        #     if target < matrix[row][col]:
-        #         r = mid - 1
-        #     elif target > matrix[row][col]:
-        #         l = mid + 1
-        #     else:
+        #     m = r - (r - l)//2
+        #     if target == matrix[row][m]:
         #         return True
-
+        #     if target < matrix[row][m]:
+        #         r = m - 1
+        #     else:
+        #         l = m + 1
         # return False
+        
 
+        # Approach 2: We can treat the 2D matrix as 1D sorted array of size m*n as all the rows are sorted
+        # To find row: array size // no. of cols
+        # To find the col: array size % no. of cols
         rows, cols = len(matrix), len(matrix[0])
-        top, bot = 0, rows - 1
-
-        while top <= bot:
-            row = top - (top - bot)//2
-            if target < matrix[row][0]:
-                bot = row - 1
-            elif target > matrix[row][-1]:
-                top = row + 1
-            else:
-                break
-            
-        # # There can be a situation where top > bot and we didn't break out from the above loop
-        # if top > bot:
-        #     return False
-
-        # We got the required row from above
-        l, r = 0, cols - 1
+        l, r = 0, (rows*cols) - 1
         while l <= r:
-            mid = r - (r - l)//2
-            if target < matrix[row][mid]:
-                r = mid - 1
-            elif target > matrix[row][mid]:
-                l = mid + 1
-            else:
+            m = r - (r - l)//2
+            row = m // cols
+            col = m % cols
+            if target == matrix[row][col]:
                 return True
-
+            if target < matrix[row][col]:
+                r = m - 1
+            else:
+                l = m + 1
+        
         return False
