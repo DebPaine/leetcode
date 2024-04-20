70b4c57800edab4f29f2a1687c48233ef4066866
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-20 13:15:26 +0530

    Time: 49 ms (16.53%), Space: 16.9 MB (22.59%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,27 +1,25 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         """
-        Time: O(logn), where n is len(nums)
+        Time: O(logn)
         Space: O(1)
-        
-        We are basically doing binary search where we are eliminating one half at a time. 
-        Instead of the standard binary search of finding some target, we are basically eliminating a half 
-        when we know that the half is sorted (is in increasing order). We then continue till we 
-        reach a single element and exit the loop. We are eliminating one sorted half at a time since we already
-        know that the first element of the sorted half will be the smallest. Also, descending elements in array
-        is not possible (given in question) so don't get confused!
+
+        Algorithm:
+        Very similar to the sorted rotated matrix problems, all have the similar pattern. We first check the left
+        sorted array if it's sorted or not. If not, then we check the right array which will definitely be sorted.
+        We then check with the first element of each array part as it will be the min element.
         """
         l, r = 0, len(nums)-1
         output = math.inf
 
         while l <= r:
-            m = r - (r-l)//2
-            # If left is less than mid, then the left half is sorted and we can eliminate that
-            if nums[l] < nums[m]:
-                output = min(output, nums[l])
-                l = m + 1
-            # If left is greater than or equal to mid, then the left half is not sorted and the right half is sorted
-            else:  # nums[l] >= nums[r] (equal sign) since in while loop l <= r and we then break out of the loop 
-                output = min(output, nums[m])
-                r = m - 1
+            m = r - (r - l)//2
+            if nums[l] < nums[m]: # check if left half is sorted or not
+                output = min(output, nums[l])   # since nums[l] will be the first element of the left sorted array
+                l = m + 1     # after finding the min, we then check the right sorted array
+            else:   # if left half is not sorted, then the right half will definitely be sorted
+                output = min(output, nums[m]) # since nums[mid] will be the first element of the right sorted array
+                r = m - 1     # after finding the min, we then check the left sorted array
+                # break     # can't use break here as nums[mid] won't be the min and there might be smaller left of it (eg: [5,1,2,3,4])
+        
         return output
