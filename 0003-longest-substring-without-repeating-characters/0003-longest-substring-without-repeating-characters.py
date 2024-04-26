2b68a5d0de1a8d5448544123ea68f17883ca6580
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-27 02:15:24 +0530

    Time: 52 ms (72.74%), Space: 16.7 MB (60.49%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Time: O(n)
+        Space: O(26) ~= O(1)
+
+        Algorithm:
+        We just have to go through the sliding window and see if we find any duplicates or not
+        in the current window. If we find duplicates, we have to iterate till there are no more
+        duplicates and carry on with the iteration.
+        """
+        chars = set()
+        output = 0
+        l = r = 0
+
+        while l <= r < len(s):
+            while s[r] in chars:    # we need to use a loop here for inputs like "aab" or "dvdf"
+                chars.remove(s[l])
+                l += 1
+
+            chars.add(s[r])
+            output = max(output, len(chars))
+            r += 1
+        
+        return output
