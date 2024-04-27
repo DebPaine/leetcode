765d65cf6a0bf90998bccfdd7edf8d9ffaf165d3
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-27 21:34:30 +0530

    Time: 58 ms (70.30%), Space: 17.7 MB (41.42%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Time: O(n)
+        Space: O(n)
+
+        Algorithm:
+        This is a classic example of using more space and getting better time complexity in return.
+        Instead of using two nested loops to go through each element in the array, we are saving
+        the current element in a dict and checking if the complement exists or not.
+        """
+        complement = defaultdict(int)
+        for i, num in enumerate(nums):
+            if target - num in complement:
+                return [complement[target - num], i]
+            complement[num] = i
+        
