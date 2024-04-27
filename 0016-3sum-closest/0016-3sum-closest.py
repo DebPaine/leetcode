bb851ade409f5bb64f3dd3954926a8271b915c01
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-27 23:57:37 +0530

    Time: 380 ms (66.08%), Space: 16.7 MB (59.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,29 +1,33 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         """
-        Time: O(n**2), where n is len(nums)
+        Time: O(n**2)
         Space: O(1)
         
-        We need to find the total which is closest to target. So basically, we need
-        to find the smallest absolute difference between target and total, and then 
-        store the total in closest variable.
+        Algorithm:
+        This is very similar to 3Sum problem. We just have to find the total which is closest to the target. 
+        We can just use the absolute value to find the "distance" between total and target (imagine a number line),
+        then we can see if it's smaller than the existing "distance" between total and existing closest or not
         """
         closest = math.inf
         nums.sort()
 
         for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:  # we don't really need this condition but this will help us in going through the duplicates again
+                continue
             l, r = i+1, len(nums)-1
-            while l < r:            
+            while l < r:
                 total = num + nums[l] + nums[r]
+                # This formula is important and very simple to understand as we only need to 
+                # find the total which is closest to our target
+                if abs(target-total) < abs(target-closest):
+                    closest = total
+
                 if total < target:
                     l += 1
                 elif total > target:
                     r -= 1
                 else:
                     return total
-                # This is wrong since we need to update closest with total
-                # closest = min(abs(target-closest), abs(target-total))
-                if abs(target-total) < abs(target-closest):
-                    closest = total
 
-        return closest 
+        return closest
