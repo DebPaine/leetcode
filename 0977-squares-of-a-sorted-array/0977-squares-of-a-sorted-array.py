df3c6576aa3269886b06f8816fe6aa2b9c932467
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-04-28 17:53:51 +0530

    Time: 152 ms (69.35%), Space: 18.6 MB (84.12%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -9,6 +9,11 @@ class Solution:
         see if the squares of left or right is larger. We then keep appending the larger of 
         left and right num to output and take reverse of it. Instead of squaring the left and
         right num, we can just take the absolute of them and compare.
+
+        Also, note that the right most value of input array is the largest value as the array
+        is sorted. We don't know if this applies to squared array since there can be a large
+        -ve number on the left pointer whose sqaure is bigger. So, we keep comparing the left and
+        right values and see whose square is greater.
         """
         l, r = 0, len(nums)-1
         output = []
