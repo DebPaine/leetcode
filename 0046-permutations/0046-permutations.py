8683276b42aa639166ec8dd9a90fba044ddab37e
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-05-04 14:11:23 +0530

    Time: 41 ms (54.51%), Space: 16.7 MB (37.60%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,8 +1,13 @@
-class Solution: 
+class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         """
-        Time: O(n!), since permutation for any number is n! and n is len(nums)
+        Time: O(n!)
         Space: O(n)
+
+        Algorithm:
+        This problem is much easier to solve by visualizing a decision tree. We have to go through nums
+        and change the ordering in-place and then add the newly ordered nums to the output. Using backtracking, 
+        we can guarantee that there will be no duplicates.
         """
         output = []
 
@@ -10,10 +15,10 @@ class Solution:
             if start >= len(nums):
                 output.append(nums[:])
                 return
-
+            
             for i in range(start, len(nums)):
                 nums[start], nums[i] = nums[i], nums[start]
-                backtrack(start+1)
+                backtrack(start + 1)
                 nums[start], nums[i] = nums[i], nums[start]
 
         backtrack(0)
