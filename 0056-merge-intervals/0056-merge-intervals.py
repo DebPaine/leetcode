378a2195bb687e3eb8a96ef1268b38db9ad5be2a
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-06-05 23:03:56 +0530

    Time: 129 ms (27.2%), Space: 21.4 MB (7.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,13 +3,23 @@ class Solution:
         intervals.sort(key=lambda x: x[0])  # sort the array based on the interval start times
         output = []
 
-        init_start, init_end = intervals[0]
-        for start, end in intervals[1:]:
-            if start <= init_end:
-                init_end = max(init_end, end)
-            else:
-                output.append([init_start, init_end])
-                init_start, init_end = start, end
+        # Method 1: using prev_start and prev_end pointers
+        # prev_start, prev_end = intervals[0]
+        # for start, end in intervals[1:]:
+        #     if start <= prev_end:
+        #         prev_end = max(prev_end, end)   # we need max in case of [[1,4], [2,3]]
+        #     else:
+        #         output.append([prev_start, prev_end])
+        #         prev_start, prev_end = start, end
+        # output.append([prev_start, prev_end]) 
+        # return output
 
-        output.append([init_start, init_end]) 
+        # Method 2
+        output = [intervals[0]]
+        for start, end in intervals[1:]:
+            previous_end = output[-1][1]
+            if start <= previous_end:  # if current start <= end of previous interval
+                output[-1][1] = max(previous_end, end)
+            else:
+                output.append([start, end])
         return output
