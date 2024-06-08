d71c0ea8c1d0e00a6eec2466ea4a8c3a0b424e68
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-06-08 21:05:08 +0530

    Time: 269 ms (27.03%), Space: 17.6 MB (97.78%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,20 +1,6 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         # Solution 1: using Counter to count the letters in the sliding window every iteration
-        p_count = Counter(p)
-        sw_count = Counter(s[:len(p)])
-        output = []
-        if sw_count == p_count:
-            output.append(0)
-        l = 0
-        for r in range(len(p), len(s)):
-            l += 1
-            sw_count = Counter(s[l:r+1])
-            if sw_count == p_count:
-                output.append(l)
-        return output
-
-        # Solution 2: add a letter count to sliding window and removing one every iteration
         # p_count = Counter(p)
         # sw_count = Counter(s[:len(p)])
         # output = []
@@ -22,11 +8,25 @@ class Solution:
         #     output.append(0)
         # l = 0
         # for r in range(len(p), len(s)):
-        #     sw_count[s[r]] += 1
-        #     sw_count[s[l]] -= 1
-        #     if sw_count[s[l]] == 0:
-        #         del sw_count[s[l]]
         #     l += 1
+        #     sw_count = Counter(s[l:r+1])
         #     if sw_count == p_count:
         #         output.append(l)
         # return output
+
+        # Solution 2: add a letter count to sliding window and removing one every iteration
+        p_count = Counter(p)
+        sw_count = Counter(s[:len(p)])
+        output = []
+        if sw_count == p_count:
+            output.append(0)
+        l = 0
+        for r in range(len(p), len(s)):
+            sw_count[s[r]] += 1
+            sw_count[s[l]] -= 1
+            if sw_count[s[l]] == 0:
+                del sw_count[s[l]]
+            l += 1
+            if sw_count == p_count:
+                output.append(l)
+        return output
