dacc64c82f158ee0097547df847fed68b101ee9a
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-06-08 19:12:00 +0530

    Time: 772 ms (31.75%), Space: 27.3 MB (64.62%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Brute force, TLE
+        """
+        Time: O(n), where n is the len(prices)
+        Space: O(1)
+        """
+        # Brute force, TLE O(n**2) time
         # max_profit = 0
         # for i in range(len(prices)):
         #     for j in range(i, len(prices)):
@@ -8,9 +12,9 @@ class Solution:
         #         max_profit = max(max_profit, profit)
         # return max_profit
 
+        # Optimised, O(n) time
         min_price, max_profit = math.inf, 0
         for price in prices:
             min_price = min(min_price, price)
             max_profit = max(max_profit, price - min_price)
         return max_profit
-        
