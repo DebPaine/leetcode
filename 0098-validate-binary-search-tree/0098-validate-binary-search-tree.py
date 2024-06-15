aa7b73423cc3710613813c3ea39af4e27cf29eaf
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-06-15 15:39:34 +0530

    Time: 44 ms (40.42%), Space: 18.2 MB (92.59%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -14,16 +14,31 @@ class Solution:
         Our first thought will be to go down the tree using dfs and compare left and right
         node values with parent node value. But we can't directly access the parent node. So,
         we need to keep track of the lower and upper limit for each node as we go down the tree.
-        Essentially we are just keeping track of the boundary conditions between which the node value is allowed.
+        Essentially, we are just keeping track of the boundary conditions between which the node value is allowed.
         """
-        def dfs(node, lower, upper):
+        # def dfs(node, lower, upper):
+        #     if not node:
+        #         return True
+        #     if not (lower < node.val < upper):
+        #         return False
+        #     left = dfs(node.left, lower, node.val)
+        #     right = dfs(node.right, node.val, upper)
+        #     return left and right
+        # return dfs(root, -math.inf, math.inf)
+
+        track = []
+        def dfs(node):
             if not node:
-                return True
-            if not (lower < node.val < upper):
-                return False
-            
-            left = dfs(node.left, lower, node.val)
-            right = dfs(node.right, node.val, upper)
-            return left and right
+                return
+
+            dfs(node.left)
+            track.append(node.val)
+            dfs(node.right) 
         
-        return dfs(root, -math.inf, math.inf)
+        dfs(root)
+
+        for i in range(1, len(track)):
+            if track[i] <= track[i-1]:
+                return False
+
+        return True
