5e026cc46ccbd95f693bcd231fe5ffc68861bb5b
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-15 23:41:34 +0530

    Time: 40 ms (91.89%), Space: 16.9 MB (43.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        """
+        Time: O(n)
+        Space: O(n)
+        """
         if len(s) != len(t):
             return False
 
         s_count = Counter(s)
         t_count = Counter(t)
-        if s_count == t_count:
+        if s_count == t_count:  # comparing two dictionaries takes O(n) time
             return True
         
         return False
