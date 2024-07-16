c8778806bff11b8b226e04d916f4a169fcd07310
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-16 23:50:10 +0530

    Time: 267 ms (65.39%), Space: 26.2 MB (17.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,6 +1,8 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
+        Time: O(n)
+        Space: O(1) using optimized solution, O(n) for unoptimized
         Algorithm:
         Find prefix sum:
         The prefix of first element will always be 1. We then iterate over nums upto len(nums)-1,
@@ -12,15 +14,26 @@ class Solution:
         easier to understand and calculate the postfixes. Postfix of the last element will always be 1.
         """
 
-        prefix, postfix, output = [1], [1], []
+        # prefix, postfix, output = [1], [1], []
+        # for num in nums[:len(nums)-1]:
+        #     prefix.append(prefix[-1]*num)
+        
+        # for num in nums[::-1][:len(nums)-1]:
+        #     postfix.append(postfix[-1]*num)
 
+        # for a, b in zip(prefix, postfix[::-1]):  # reverse postfix again to get the correct order
+        #     output.append(a*b)
+        # return output
+
+        # Optimized O(1) space
+        output = [1]
+        # Calculate prefix
         for num in nums[:len(nums)-1]:
-            prefix.append(prefix[-1]*num)
-        
-        for num in nums[::-1][:len(nums)-1]:
-            postfix.append(postfix[-1]*num)
+            output.append(output[-1]*num)
 
-        for a, b in zip(prefix, postfix[::-1]):
-            output.append(a*b)
-        
+        # Calculate postfix and multiply to prefix
+        postfix = 1
+        for i in range(len(nums)-1, 0, -1):  # iterate in reverse
+            postfix *= nums[i]
+            output[i-1] *= postfix
         return output
