4a800789ff9c8142d3ef8e4274556adeccef62d7
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-16 22:25:18 +0530

    Time: 77 ms (96.86%), Space: 22.3 MB (5.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        Time: O(n)
+        Space: O(n)
+
+        Algorithm:
+        We are using bucket sort and storing the nums in different buckets based on their count. We use
+        the bucket indices for the count. Once the nums are in appropriate buckets, we then iterate over the
+        buckets array in reverse so that we get the most frequent elements first. We then return only the 
+        top k elements as per the requirement.
+        """
+        
         buckets = [[] for _ in range(len(nums) + 1)]
         output = []
         num_count = Counter(nums)
