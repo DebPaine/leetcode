bff75138e69f7f00614f0134dc3b5e72076e04b6
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-17 23:20:42 +0530

    Time: 37 ms (91.1%), Space: 17.4 MB (36.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+        Time: O(n)
+        Space: O(n), since we are storing the cleaned string
+
+        Algorithm:
+        Easy problem, we just need to clean all the non alphanumeric characters and lowercase the string.
+        We then need to use the left and right pointer on either end of the cleaned string and if both the 
+        pointers have the same characters or not.
+        """
         cleaned_s = "".join([char for char in s if char.isalnum()]).lower()
 
         l, r = 0, len(cleaned_s) - 1
