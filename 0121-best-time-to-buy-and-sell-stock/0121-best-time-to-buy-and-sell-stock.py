aed70273e7ace431adc1ad39bb837d6f5733cbd5
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-20 16:41:33 +0530

    Time: 771 ms (32.08%), Space: 27.4 MB (21.74%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = math.inf
-        max_profit = -math.inf
+        """
+        Time: O(n), where n is the len(prices)
+        Space: O(1)
+        
+        Algorithm:
+        We can use the standard brute force approach of going through each of the prices and then
+        computing what's the max profit. Else, we can use the pointer based approach where we keep
+        track of the min price and calculate the max profit against it.
+        """
+        # Brute force, TLE O(n**2) time
+        # max_profit = 0
+        # for i in range(len(prices)):
+        #     for j in range(i, len(prices)):
+        #         profit = prices[j] - prices[i]
+        #         max_profit = max(max_profit, profit)
+        # return max_profit
 
+        # Optimised, O(n) time
+        min_price, max_profit = math.inf, 0
         for price in prices:
             min_price = min(min_price, price)
             max_profit = max(max_profit, price - min_price)
-
         return max_profit
