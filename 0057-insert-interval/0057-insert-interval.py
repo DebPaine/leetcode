b9cfbcbc0306e82ac6b2c51844547aaf30f23f96
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-23 00:49:09 +0530

    Time: 74 ms (63.37%), Space: 19.8 MB (54.79%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,5 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        """
+        Time: O(n)
+        Space: O(n), if we include the output array
+
+        Algorithm:
+        This problem seems easy but it's actually difficult to get the conditions right. We have to think about this
+        step by step:
+        1. Iterate through each of the intervals one by one.
+        2. For each interval, we compare it with the newInterval with the below conditions.
+        3. If the first two conditions don't execute, that means there is an overlap between newInterval and the current
+        interval. Hence, we save this new merged interval in newInterval itself so that in the next iterations we can use it
+        and see if there are any other further overlaps or not.
+        4. At the end, we append newInterval before returning the output since there is a chance we never return from within 
+        the first condition in the loop.
+        """
+
         output = []
 
         for i in range(len(intervals)):
