b452277e031018c1d046bfb4e7f246de387a63fa
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-23 20:29:55 +0530

    Time: 134 ms (13.18%), Space: 21.2 MB (20.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,34 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        """
-        Time: O(nlogn)
-        Space: O(1), not counting the output array
+        intervals.sort()
+        merged = [intervals[0]]
 
-        Algorithm:
-        Pretty simple problem. We have to sort the intervals first based on the start values. We then see
-        if the current interval's start is less than the previous interval's end. If they are, then we have
-        an overlap, else we have separate intervals.
-        """
-        intervals.sort(key=lambda x: x[0])  # sort the array based on the interval start times
-        output = []
-
-        # Method 1: using prev_start and prev_end pointers
-        # prev_start, prev_end = intervals[0]
-        # for start, end in intervals[1:]:
-        #     if start <= prev_end:
-        #         prev_end = max(prev_end, end)   # we need max in case of [[1,4], [2,3]]
-        #     else:
-        #         output.append([prev_start, prev_end])
-        #         prev_start, prev_end = start, end
-        # output.append([prev_start, prev_end]) 
-        # return output
-
-        # Method 2: not using any pointers
-        output = [intervals[0]]
         for start, end in intervals[1:]:
-            previous_end = output[-1][1]
-            if start <= previous_end:  # if current start <= end of previous interval
-                output[-1][1] = max(previous_end, end)
+            prev_start, prev_end = merged[-1]
+            if start <= prev_end:
+                merged[-1] = [prev_start, max(prev_end, end)]
             else:
-                output.append([start, end])
-        return output
+                merged.append([start, end])
+        
+        return merged
