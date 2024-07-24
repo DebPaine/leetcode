85037ce04d1495871054bddb9ec7402dda097cc0
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-24 22:25:37 +0530

    Time: 66 ms (80.56%), Space: 16.6 MB (98.13%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -1,3 +1,7 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        """
+        Time: O(n), where n is len(nums)
+        Space: O(1)
+        """
         return nums*2
