ea5f6cc59d0bd1346d999b6fc7788cc448ef02d4
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-24 22:50:16 +0530

    Time: 550 ms (9.32%), Space: 18.5 MB (14.99%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -1,9 +1,19 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
+        """
+        Time: O(n)
+        Space: O(1), not counting the output array
+
+        Algorithm:
+        We can go through the array in reverse as this will be easier for us to calculate the greatest right
+        side element of every element in the array. 
+        """
+
         output = [-1]
 
-        for num in arr[::-1][:len(arr)-1]:
-            current_max = max(output[-1], num)
+        # len(arr)-1 since we have to finish one element before as this will be the current element's greatest right side
+        for num in arr[::-1][:len(arr)-1]:  
+            current_max = max(output[-1], num)  # since output[-1] will always contain the current max value on the right side
             output.append(current_max)
         
         return output[::-1]
