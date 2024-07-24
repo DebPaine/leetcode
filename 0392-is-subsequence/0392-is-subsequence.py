43b2f667a898b43830a9d35d80e9afbf78ebad2e
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-24 23:13:31 +0530

    Time: 31 ms (84.81%), Space: 16.7 MB (6.16%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,14 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        """
-        Time: O(n), where n is len(t)
-        Space: O(1)
-        """
-        ptr1, ptr2 = 0, 0
+        ptr1 = ptr2 = 0
 
-        while ptr1 < len(s) and ptr2 < len(t):  # check ptr1 and ptr2 since s can be an empty string
+        while ptr1 < len(s) and ptr2 < len(t):
             if s[ptr1] == t[ptr2]:
                 ptr1 += 1
             ptr2 += 1
-
+        
         return ptr1 == len(s)
+            
