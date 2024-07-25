55bf7495aca5c41b2478cc70a70be5e55add744d
DebPaine <49160622+DebPaine@users.noreply.github.com>
2024-07-25 21:53:46 +0530

    Time: 95 ms (51.95%), Space: 16.6 MB (27.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,5 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        """
+        Time: O(n), where n is len(s)
+        Space: O(26)~=O(1), as we can only have 26 uppercase alphabets
+
+        Steps:
+        1. Either iterate over the string using for or while loop using sliding window technique.
+        2. First calculate the length of the sliding window.
+        3. Then see what is the count of all characters in the sliding window.
+        4. Then check which char occurred max no. of times in the sliding window.
+        5. Now find out how many chars we have to replace in the sliding window to make the substring have same letters.
+        6. Now see if the no. of replacements is greater than k or not. If it's greater, then move left pointer by one
+        and remove one occurence of left char from the sliding window. Else, we can consider this as the longest substring.
+        7. Keep iterating through the string by increasing the right pointer by one.
+        """
         char_count = [0]*26
         output = 0
 
